441f21d3a9ac8632f793706ef4b2c2a2ef004923
Riya Bodwani <riyabodwani02@gmail.com>
2026-08-16 01:03:53 +0530

    Serve React frontend from backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'dist')))
 
 // 3.8 - Custom Morgan token for request body
 morgan.token('body', request => {
